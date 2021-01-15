8edd5e21d70b820f94dcf77fdecb659122a85434
Rene Barto <rene.barto66@gmail.com>
2021-01-15 16:26:47 +0100

    Fix build on Linux

--- code/libraries/osal/include/osal/Utilities.h
+++ code/libraries/osal/include/osal/Utilities.h
@@ -69,9 +69,11 @@ template<> inline std::uint64_t SwapBytes<std::uint64_t>(std::uint64_t value)
 #endif
 }
 
+#if !defined (PLATFORM_LINUX)
 template<> inline unsigned long SwapBytes<unsigned long>(unsigned long value)
 {
     return SwapBytes<std::uint32_t>(value);
 }
+#endif
 
 } // namespace osal
